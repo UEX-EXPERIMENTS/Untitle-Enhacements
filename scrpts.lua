278dcbbd5a7905e1cdcf18ac71cc9600f9f54da9
UEX-EXPERIMENTS <assawuas@gmail.com>
2026-02-27 00:23:14 -0500

    Create scrpts.lua

---
+++ scrpts.lua
@@ -0,0 +1,6 @@
+local Bracket = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/BracketV3/main/BracketV3.lua"))()
+local Window = Bracket:CreateWindow("Fabian Enhancements X - $5 Edition", {
+    Color = Color3.fromRGB(147, 112, 219),  -- Morado principal como tu dibujo
+    Size = UDim2.new(0, 650, 0, 450),
+    Theme = "Dark"
+})
